2559bb79bd9600aa453ec92d6d3a40c7ae89da73
eliasakesson <akessonelias@gmail.com>
2024-06-30 23:45:27 +0200

    Error fix: Added suspense  tag to cases page

--- app/(pages)/cases/page.tsx
+++ app/(pages)/cases/page.tsx
@@ -1,6 +1,7 @@
 import { getCases } from '@/app/utils/getCase';
 import Client from './Client';
 import { getCasesProps } from '@/app/utils/getLandingPage';
+import { Suspense } from 'react';
 
 const Cases = async () => {
 	const props = await getCasesProps();
@@ -8,7 +9,9 @@ const Cases = async () => {
 
 	return (
 		<div id="cases" className="w-full bg-white py-32">
-			<Client props={props} cases={cases} />
+			<Suspense>
+				<Client props={props} cases={cases} />
+			</Suspense>
 		</div>
 	);
 };
